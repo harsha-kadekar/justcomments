6292bb44e688adf0f23250ffb0b468da5fbcef04
Harsha Kadekar <harsha.kadekar@gmail.com>
2019-09-24 22:52:42 -0700

    project setup

---
+++ src/main/java/com/hkadekar/justcomments/JustcommentsApplication.java
@@ -0,0 +1,13 @@
+package com.hkadekar.justcomments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustcommentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustcommentsApplication.class, args);
+    }
+
+}
